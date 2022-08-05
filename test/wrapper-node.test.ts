e1bc0d9197d697c4f518e125adb9d077ebfb6203
Frank Kulak <fs.kulak@gmail.com>
2022-08-05 14:47:14 -0400

    Add stubs for wrapper node tests.

--- test/wrapper-node.test.ts
+++ test/wrapper-node.test.ts
@@ -3,7 +3,92 @@ import type { XmlNode } from "../dst/xml";
 import { XmlDocumentNode, XmlElementNode, XmlValueNode, XmlCommentNode } from "../dst/xml";
 
 describe("XmlWrapperNode", () => {
-  describe("", () => {
+  //#region Initialization
 
+  describe("#constructor", () => {
+    // TODO:
   });
+
+  //#endregion Initialization
+
+  //#region Properties
+
+  describe("#attributes", () => {
+    // TODO:
+  });
+
+  describe("#child", () => {
+    // TODO:
+  });
+
+  describe("#children", () => {
+    // TODO:
+  });
+
+  describe("#hasChildren", () => {
+    // TODO:
+  });
+
+  describe("#id", () => {
+    // TODO:
+  });
+
+  describe("#innerValue", () => {
+    // TODO:
+  });
+
+  describe("#name", () => {
+    // TODO:
+  });
+
+  describe("#numChildren", () => {
+    // TODO:
+  });
+
+  describe("#tag", () => {
+    // TODO:
+  });
+
+  describe("#type", () => {
+    // TODO:
+  });
+
+  describe("#value", () => {
+    // TODO:
+  });
+
+  //#endregion Properties
+
+  //#region Methods
+
+
+  describe("#addChildren()", () => {
+    // TODO:
+  });
+
+  describe("#addClones()", () => {
+    // TODO:
+  });
+
+  describe("#clone()", () => {
+    // TODO:
+  });
+
+  describe("#deepSort()", () => {
+    // TODO:
+  });
+
+  describe("#findChild()", () => {
+    // TODO:
+  });
+
+  describe("#sort()", () => {
+    // TODO:
+  });
+
+  describe("#toXml()", () => {
+    // TODO:
+  });
+
+  //#endregion Methods
 });
